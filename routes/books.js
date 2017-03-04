87b273b2157c81ff4fcfe8227b2ea2e96778b79a
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-03-03 17:09:00 -0800

    got router calls working

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,18 @@
 'use strict';
 
 const express = require('express');
+const env = process.env.node_env || 'test';
+const config = require('../knexfile.js')[env];
+const knex = require('knex')(config);
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
 // YOUR CODE HERE
+router.get('/books', function(req, res){
+  knex('books').then(function(books){
+    res.status(200).json(books);
+  })
+})
 
 module.exports = router;
